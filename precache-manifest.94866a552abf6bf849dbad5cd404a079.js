e6474d6892b536fd91dbbb28184e70c9a645d078
zhao19991111 <49088390+zhao19991111@users.noreply.github.com>
2020-02-04 23:35:55 -0800

    Updates

---
+++ precache-manifest.94866a552abf6bf849dbad5cd404a079.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753a97d5884c1eaf881882720acaa531",
+    "url": "/experiment/index.html"
+  },
+  {
+    "revision": "325bdc9c93a06fb9f6f8",
+    "url": "/experiment/static/css/main.3c386395.chunk.css"
+  },
+  {
+    "revision": "31d1c8ed69492c28d999",
+    "url": "/experiment/static/js/2.83227235.chunk.js"
+  },
+  {
+    "revision": "f25fa93e176aba09314d77bb1a3fff23",
+    "url": "/experiment/static/js/2.83227235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "325bdc9c93a06fb9f6f8",
+    "url": "/experiment/static/js/main.b170109b.chunk.js"
+  },
+  {
+    "revision": "b9798919ec598afa8230",
+    "url": "/experiment/static/js/runtime-main.5ab5b131.js"
+  },
+  {
+    "revision": "c34d14fb876f9a30a9ee178b3679481f",
+    "url": "/experiment/static/media/1.c34d14fb.jpg"
+  },
+  {
+    "revision": "5ae968ec4f25bdf251178aa103f0f049",
+    "url": "/experiment/static/media/11.5ae968ec.jpg"
+  },
+  {
+    "revision": "c342a30a581a44aca3cf015278bad599",
+    "url": "/experiment/static/media/2.c342a30a.jpg"
+  },
+  {
+    "revision": "5915654466d72ea0232cf41e91c29f83",
+    "url": "/experiment/static/media/22.59156544.jpg"
+  },
+  {
+    "revision": "d29b2fad8a2fd70b5cb1fc2b44977e9c",
+    "url": "/experiment/static/media/3.d29b2fad.jpg"
+  },
+  {
+    "revision": "80a0941ea7d9ca347ff6d8f1aec9b08b",
+    "url": "/experiment/static/media/33.80a0941e.jpg"
+  },
+  {
+    "revision": "c7366cfed2ebe57c4f12de7b079d6c49",
+    "url": "/experiment/static/media/4.c7366cfe.jpg"
+  },
+  {
+    "revision": "6e5caa4755248518b56dd6a0d06d74cf",
+    "url": "/experiment/static/media/44.6e5caa47.jpg"
+  },
+  {
+    "revision": "3f7b0bcb957b1575cb217b8c9998b46b",
+    "url": "/experiment/static/media/5.3f7b0bcb.jpg"
+  },
+  {
+    "revision": "d0b0699a9b0b2dc866d1e5658a5cc380",
+    "url": "/experiment/static/media/6.d0b0699a.jpg"
+  },
+  {
+    "revision": "a3e20ffc61acdb41db8cd9986f2a59d0",
+    "url": "/experiment/static/media/Forza.a3e20ffc.jpg"
+  },
+  {
+    "revision": "f9901cdf40d960124f2e0aaf82ce2166",
+    "url": "/experiment/static/media/Night.f9901cdf.jpg"
+  },
+  {
+    "revision": "4fbfbd6af7f2125842bac33c2a3868fe",
+    "url": "/experiment/static/media/Night2.4fbfbd6a.jpg"
+  },
+  {
+    "revision": "d113879984da1325004f1076033ae9dc",
+    "url": "/experiment/static/media/a.d1138799.jpg"
+  },
+  {
+    "revision": "c503b6ca723e7911e18606f8274f4dee",
+    "url": "/experiment/static/media/b.c503b6ca.jpg"
+  },
+  {
+    "revision": "c9232387364ecc3a5f0bba81fe57471c",
+    "url": "/experiment/static/media/c.c9232387.jpg"
+  },
+  {
+    "revision": "c7f917badb930351a498224c6d85b326",
+    "url": "/experiment/static/media/comment_data.c7f917ba.txt"
+  },
+  {
+    "revision": "b71f38cd2e671cf62183cd540cda4f25",
+    "url": "/experiment/static/media/d.b71f38cd.jpg"
+  },
+  {
+    "revision": "7235a0728b06d3252c3a607ca3a75390",
+    "url": "/experiment/static/media/e.7235a072.jpg"
+  },
+  {
+    "revision": "369c709a53dd720dc14cd009f6d9b829",
+    "url": "/experiment/static/media/f.369c709a.jpg"
+  },
+  {
+    "revision": "9a95f45ed6b067eff4373eb33a54d356",
+    "url": "/experiment/static/media/player.9a95f45e.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/experiment/precache-manifest.94866a552abf6bf849dbad5cd404a079.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/experiment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
